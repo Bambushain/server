d77a0483fbfcd5d78bc6fdafa88890cccb59d0cd
Imanuel Ulbricht <imanuel.ulbricht@bambushain.app>
2025-01-04 02:24:05 +0100

    WEB-1 Rebuild in Leptos
    Add new deployment for review environments

--- backend/api/src/app.rs
+++ backend/api/src/app.rs
@@ -1,52 +1,8 @@
 use actix_web::{middleware, App, HttpServer};
-use bamboo_common::backend::dbal;
-use bamboo_common::backend::migration::{Migrator, MigratorTrait};
 use bamboo_common::backend::services::DbConnection;
 use bamboo_common::backend::services::MinioClient;
 
 use crate::routes;
-
-async fn setup_google_playstore_grove(
-    user_id: i32,
-    db: &sea_orm::DatabaseConnection,
-) -> std::io::Result<()> {
-    if !dbal::grove_exists_by_name("Google".to_string(), db)
-        .await
-        .map_err(std::io::Error::other)?
-    {
-        dbal::create_grove("Google".to_string(), false, user_id, db)
-            .await
-            .map_err(std::io::Error::other)
-            .map(|_| ())
-    } else {
-        Ok(())
-    }
-}
-
-async fn setup_google_playstore_user(db: &sea_orm::DatabaseConnection) -> std::io::Result<()> {
-    let email = "playstore@google.bambushain".to_string();
-    let password = "NkWHoLDmzg4aVEx".to_string();
-
-    if let Ok(user) = dbal::get_user_by_email_or_username(email.clone(), db).await {
-        dbal::set_password(user.id, password, db)
-            .await
-            .map_err(std::io::Error::other)
-            .map(|_| ())
-    } else {
-        let user = dbal::create_user(
-            bamboo_common::core::entities::User::new(
-                email,
-                "Google Playstore".to_string(),
-                "google".to_string(),
-            ),
-            password,
-            db,
-        )
-        .await
-        .map_err(std::io::Error::other)?;
-        setup_google_playstore_grove(user.id, db).await
-    }
-}
 
 pub async fn start_server() -> std::io::Result<()> {
     bamboo_common::backend::logging::init();
@@ -56,18 +12,7 @@ pub async fn start_server() -> std::io::Result<()> {
         .await
         .map_err(std::io::Error::other)?;
 
-    let migrations = Migrator::get_pending_migrations(&db)
-        .await
-        .map_err(std::io::Error::other)?;
-    log::info!("Running {} migrations", migrations.len());
-
-    Migrator::up(&db, None)
-        .await
-        .map_err(std::io::Error::other)?;
-    log::info!("Successfully migrated database");
     let minio_client = MinioClient::new().map_err(std::io::Error::other)?;
-
-    setup_google_playstore_user(&db).await?;
 
     HttpServer::new(move || {
         App::new()
---
+++ src/bin/migrator.rs
@@ -0,0 +1,66 @@
+use bamboo_common::backend::dbal;
+use bamboo_common::backend::migration::{sea_orm, Migrator, MigratorTrait};
+
+async fn setup_google_playstore_grove(
+    user_id: i32,
+    db: &sea_orm::DatabaseConnection,
+) -> std::io::Result<()> {
+    if !dbal::grove_exists_by_name("Google".to_string(), db)
+        .await
+        .map_err(std::io::Error::other)?
+    {
+        dbal::create_grove("Google".to_string(), false, user_id, db)
+            .await
+            .map_err(std::io::Error::other)
+            .map(|_| ())
+    } else {
+        Ok(())
+    }
+}
+
+async fn setup_google_playstore_user(db: &sea_orm::DatabaseConnection) -> std::io::Result<()> {
+    let email = "playstore@google.bambushain".to_string();
+    let password = "NkWHoLDmzg4aVEx".to_string();
+
+    if let Ok(user) = dbal::get_user_by_email_or_username(email.clone(), db).await {
+        dbal::set_password(user.id, password, db)
+            .await
+            .map_err(std::io::Error::other)
+            .map(|_| ())
+    } else {
+        let user = dbal::create_user(
+            bamboo_common::core::entities::User::new(
+                email,
+                "Google Playstore".to_string(),
+                "google".to_string(),
+            ),
+            password,
+            db,
+        )
+        .await
+        .map_err(std::io::Error::other)?;
+        setup_google_playstore_grove(user.id, db).await
+    }
+}
+
+#[actix::main]
+async fn main() -> std::io::Result<()> {
+    bamboo_common::backend::logging::init();
+
+    log::info!("Open the bamboo grove");
+    let db = bamboo_common::backend::database::get_database()
+        .await
+        .map_err(std::io::Error::other)?;
+
+    let migrations = Migrator::get_pending_migrations(&db)
+        .await
+        .map_err(std::io::Error::other)?;
+    log::info!("Running {} migrations", migrations.len());
+
+    Migrator::up(&db, None)
+        .await
+        .map_err(std::io::Error::other)?;
+    log::info!("Successfully migrated database");
+
+    setup_google_playstore_user(&db).await
+}
